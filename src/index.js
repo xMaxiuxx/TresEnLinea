bee3b7ff22cc55ca68ed749acacc3683a2dcfa1f
xMaxiuxx <maxik4po@gmail.com>
2024-01-15 15:42:28 -0300

    Game-Complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import React from 'react';//Importa React
+import ReactDOM from 'react-dom/client';// Biblioteca de React para hablar con los navegadores web (React DOM)
+
+import './styles.css'; //los estilos para tus componentes
+
+import App from './App';// el componente que creaste en App.js.
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,3 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
+console.log({ReactDOM})
